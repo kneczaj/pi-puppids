386bf68b2b4f3b1ebcbfc4f428d2caf54b67adee
Markus Fensterer <markus.fensterer@gmail.com>
2012-12-06 01:09:14 +0100

    establish web socket connection between client and server, use the ClientPushActor to keep track of currently connected Players and to fan out information of location changes of players to all web sockets

--- wars/app/controllers/Location.java
+++ wars/app/controllers/Location.java
@@ -13,6 +13,9 @@ import org.bson.types.ObjectId;
 
 import play.mvc.Controller;
 import play.mvc.Result;
+
+import communication.ClientPushActor;
+
 import daos.PlayerDAO;
 import daos.PlayerLocationDAO;
 
@@ -20,30 +23,34 @@ public class Location extends Controller {
 
 	private static PlayerLocationDAO locationDAO = new PlayerLocationDAO();
 	private static PlayerDAO playerDAO = new PlayerDAO();
-	
-	public static Result updateLocation(String playerId, String lat, String lng, String uncertainty, String speed, String timestamp) {
+
+	public static Result updateLocation(String playerId, String lat,
+			String lng, String uncertainty, String speed, String timestamp) {
+		
 		Player p = playerDAO.get(new ObjectId(playerId));
 		if (p == null) {
 			return badRequest();
 		}
 		
+		Double latitude = new Double(lat);
+		Double longitude = new Double(lng);
+		Integer sp = (speed == null || speed.equals("")) ? null : new Integer(speed);
+		Integer accuracy = (uncertainty == null || uncertainty.equals("")) ? null : new Integer(uncertainty);
+		
 		PlayerLocation pl = new PlayerLocation();
 		pl.setPlayer(p);
-		pl.setLatitude(new Double(lat));
-		pl.setLongitude(new Double(lng));
-		
-		if (uncertainty != null && !uncertainty.equals("")) 
-			pl.setUncertainty(new Integer(uncertainty));
-		
-		if (speed != null && !speed.equals("")) 
-			pl.setSpeed(new Integer(speed));
-		
+		pl.setLatitude(latitude);
+		pl.setLongitude(longitude);
+		pl.setUncertainty(accuracy);
+		pl.setSpeed(sp);
 		pl.setTimestamp(new Date(new Long(timestamp)));
 		locationDAO.save(pl);
+
+		// inform other player of location change
+		ClientPushActor.locationChanged(pl.getId().toString(), new Long(timestamp), longitude, latitude, accuracy, sp);
 		
 		Map<String, String> d = new HashMap<String, String>();
 		d.put("success", "foo");
 		return ok(toJson(d));
 	}
-
 }
