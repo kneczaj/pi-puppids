5239434cf5688f444bb3fb7a6171324e42074d3e
Peter Retzer <peter.retzer@gmail.com>
2012-12-12 17:30:54 +0100

    added first version of the 'Edit profile' feature

--- wars/app/controllers/Application.java
+++ wars/app/controllers/Application.java
@@ -5,6 +5,7 @@ import play.mvc.Controller;
 import play.mvc.Result;
 import securesocial.core.java.SecureSocial;
 import views.html.index;
+import views.html.profile;
 
 import com.google.inject.Inject;
 
@@ -27,6 +28,13 @@ public class Application extends Controller {
 		
 		samplePlayer = playerDAO.findOne("username", "sepp");
 		return ok(index.render(samplePlayer));
+	}
+	
+	@SecureSocial.SecuredAction
+	public static Result profile() {
+		SocialUser user = (SocialUser) ctx().args.get(SecureSocial.USER_KEY);
+		Player player = playerDAO.findOne("email", user.getEmail());
+		return ok(profile.render(player));
 	}
 
 }
