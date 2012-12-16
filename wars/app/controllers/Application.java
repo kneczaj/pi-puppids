ada6c20aeda9866750683ff0f0c2c411b90a1a75
kamil <kamil@kneczaj-hp.tu-muenchen.de>
2012-12-17 00:46:12 +0100

    Presenting the team Invitation form Checking existence of email of the invited player

--- wars/app/controllers/Application.java
+++ wars/app/controllers/Application.java
@@ -1,7 +1,11 @@
 package controllers;
 
+import java.util.List;
+
 import models.Player;
 import models.PlayerLocation;
+import models.Team;
+
 import play.mvc.Controller;
 import play.mvc.Result;
 import securesocial.core.java.SecureSocial;
@@ -11,8 +15,12 @@ import com.google.inject.Inject;
 
 import daos.PlayerDAO;
 import daos.PlayerLocationDAO;
+import daos.TeamDAO;
+
 import views.html.index;
 import views.html.profile;
+
+import services.api.TeamService;
 
 public class Application extends Controller {
 
@@ -22,13 +30,23 @@ public class Application extends Controller {
 	@Inject
 	private static PlayerLocationDAO playerLocationDAO;
 	
+	@Inject
+	private static TeamDAO teamDAO;
+	
+	@Inject
+	private static TeamService teamService;
+	
 	@SecureSocial.SecuredAction
 	public static Result index() {
 		SocialUser user = (SocialUser) ctx().args.get(SecureSocial.USER_KEY);
 		Player player = playerDAO.findOne("email", user.getEmail());
+		
+		Team team = player.getTeam();
+		List<Player> teammates = teamService.getMembers(team);
+		
 		PlayerLocation playerLocation = playerLocationDAO.findLatestLocation(player);
 		
-		return ok(index.render(player, playerLocation));
+		return ok(index.render(player, playerLocation, teammates));
 	}
 	
 	@SecureSocial.SecuredAction
