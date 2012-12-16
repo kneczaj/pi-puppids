d5cc6f3aedc41105762f5fc539ca4df3a6b86f57
Michael Sch?tzlein <schaetzl@in.tum.de>
2012-12-16 14:48:06 +0100

    added mapping from PlaceType to ResourceType

--- wars/app/models/ResourceType.java
+++ wars/app/models/ResourceType.java
@@ -1,9 +1,11 @@
 package models;
 
 public enum ResourceType {
-	Credits,
-	Food,
-	Transportation,
-	Knowledge,
-	Material
+	Credits,		//Money
+	Cultural,		//Cultural
+	Food,			//Food
+	Transportation,	//Transportation
+	Knowledge,		//Knowledge
+	Material,		//Building Material
+	Special,		//Special places
 }
