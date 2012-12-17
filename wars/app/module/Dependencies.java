dd7883fcde0eb768ffbc5a84622b1237d517535f
kamil <kamil@kneczaj-hp.samsung.router>
2012-12-17 15:33:27 +0100

    missing files...

---
+++ wars/app/module/Dependencies.java
@@ -0,0 +1,63 @@
+package module;
+
+
+import services.api.LocationTrackingService;
+import services.api.MapInfoService;
+import services.api.ResourceService;
+import services.api.ScoreService;
+import services.api.TeamService;
+import services.dummy.ResourceServiceDummyImpl;
+import services.dummy.ScoreServiceDummyImpl;
+import services.impl.LocationTrackingServiceImpl;
+import services.impl.MapInfoServiceImpl;
+import services.impl.TeamServiceImpl;
+
+import com.google.code.morphia.Morphia;
+import com.google.inject.Binder;
+import com.google.inject.Module;
+import com.google.inject.Singleton;
+import com.mongodb.Mongo;
+
+import daos.AccessTokenDAO;
+import daos.CityDAO;
+import daos.FactionDAO;
+import daos.PlaceDAO;
+import daos.PlayerDAO;
+import daos.PlayerLocationDAO;
+import daos.ResourceDepotDAO;
+import daos.TeamDAO;
+import daos.TeamInviteDAO;
+import daos.UnitDAO;
+
+/**
+ * Configures Google Guice to inject the proper implementations for certain interfaces.
+ * 
+ * @author markus
+ */
+public class Dependencies implements Module {
+
+	@Override
+	public void configure(Binder binder) {
+		// bind service interfaces to concrete implementations
+		binder.bind(MapInfoService.class).to(MapInfoServiceImpl.class).in(Singleton.class);
+		binder.bind(LocationTrackingService.class).to(LocationTrackingServiceImpl.class).in(Singleton.class);
+		binder.bind(TeamService.class).to(TeamServiceImpl.class).in(Singleton.class);
+		binder.bind(ResourceService.class).to(ResourceServiceDummyImpl.class).in(Singleton.class);
+		binder.bind(ScoreService.class).to(ScoreServiceDummyImpl.class).in(Singleton.class);
+	
+		binder.bind(Morphia.class);
+		binder.bind(Mongo.class);
+		
+		binder.bind(PlayerDAO.class).in(Singleton.class);
+		binder.bind(TeamDAO.class).in(Singleton.class);
+		binder.bind(TeamInviteDAO.class).in(Singleton.class);
+		binder.bind(FactionDAO.class).in(Singleton.class);
+		binder.bind(CityDAO.class).in(Singleton.class);
+		binder.bind(UnitDAO.class).in(Singleton.class);
+		binder.bind(AccessTokenDAO.class).in(Singleton.class);
+		binder.bind(PlaceDAO.class).in(Singleton.class);
+		binder.bind(PlayerLocationDAO.class).in(Singleton.class);
+		binder.bind(ResourceDepotDAO.class).in(Singleton.class);
+	}
+
+}
---
+++ wars/app/services/api/TeamService.java
@@ -0,0 +1,63 @@
+package services.api;
+
+import java.util.List;
+
+import models.City;
+import models.Faction;
+import models.Invitation;
+import models.Player;
+import models.Team;
+
+/**
+ * Manage Teams
+ * 
+ * @author markus
+ */
+public interface TeamService {
+
+	/**
+	 * Create a team
+	 * 
+	 * @param player
+	 * @return
+	 */
+	public Team createTeam(Faction faction, City city, String name);
+
+	/**
+	 * List the members of a team
+	 * 
+	 * @param team
+	 * @return
+	 */
+	public List<Player> getMembers(Team team);
+
+	/**
+	 * Invite a player to join an other team.
+	 * 
+	 * @param sender player who invites to their team
+	 * @param receiver player invited
+	 */
+	public Invitation invite(Player sender, Player receiver);
+	
+	/**
+	 * A Player may accept an invite to a team
+	 * 
+	 * @param player 
+	 * @param team the team he was invited to
+	 * @return the refreshed player entity
+	 */
+	public Player acceptInvite(Invitation invitation);
+
+//	/**
+//	 * Invite strangers (people that are not already registered at ARWars) via
+//	 * emailAdress to a team. Sends an email to the adress with an invitation
+//	 * link.
+//	 * 
+//	 * @param emailAdress
+//	 * @param team
+//	 */
+//	public void iniviteStranger(String emailAdress, Team team);
+	
+	public void sendInvitation(Invitation invitation);
+
+}
