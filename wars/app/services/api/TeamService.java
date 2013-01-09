ce88bef0a5e7ffd861bb580c10bdf3e8137027e4
Michael Sch?tzlein <schaetzl@in.tum.de>
2013-01-09 17:07:20 +0100

    Enhanced ScoreServiceImpl, now counts Resources and Units, too

---
+++ wars/app/services/api/TeamService.java
@@ -0,0 +1,118 @@
+package services.api;
+
+import services.api.error.TeamServiceException;
+import models.City;
+import models.Faction;
+import models.Invitation;
+import models.Player;
+import models.Team;
+
+/**
+ * Manage Teams
+ * 
+ * @author markus
+ */
+public interface TeamService extends Service {
+
+	/**
+	 * Create a team
+	 * 
+	 * @param player
+	 * @return
+	 */
+	public Team createTeam(Faction faction, City city, String name);
+
+	/**
+	 * Create a team
+	 * 
+	 * @param city
+	 * @param name
+	 * @return
+	 */
+	public Team createTeam(City city, String name);
+	
+	
+	/**
+	 * Join a player to a team.
+	 * 
+	 * @param player
+	 *            player who joins
+	 * @param team
+	 *            team player joins to
+	 */
+	public Player joinTeam(Player player, Team team);
+
+	/**
+	 * Invite a player to join an other team.
+	 * 
+	 * @param sender
+	 *            player who invites to their team
+	 * @param receiver
+	 *            player invited
+	 */
+	public Invitation invite(Player sender, Player receiver);
+
+	/**
+	 * A Player may accept an invite to a team
+	 * 
+	 * @param token
+	 * @param loggedPlayer
+	 * @returns 
+	 * 		logged player instance after accepting the invitaiton  
+	 * @throws TeamServiceException
+	 * 		with problems encountered in the exception message
+	 */
+	public Player acceptInvitation(Invitation invitation, Player loggedPlayer) 
+			throws TeamServiceException;
+	
+	/**
+	 * Finds invitation with given token in the db
+	 * 
+	 * @param token
+	 * @return
+	 * @throws TeamServiceException
+	 * 		if invitation was not found
+	 */
+	public Invitation getInvitation(String token) throws TeamServiceException;
+
+	/**
+	 * Invite strangers (people that are not already registered at ARWars) via
+	 * emailAdress to a team. Sends an email to the adress with an invitation
+	 * link.
+	 * 
+	 * @param sender
+	 * @param emailAdress
+	 */
+	public Invitation iniviteStranger(Player sender, String emailAddress);
+	
+	/**
+	 * makes invitation from given data
+	 * 
+	 * @param invitedUserOrEmail
+	 * 		a string of a user name of an email to invite
+	 * @param loggedPlayer
+	 * @return Invitation
+	 * 		the invitation - already stored in the db
+	 * 
+	 * @throws TeamServiceException
+	 * 		with problems encountered in the exception message 
+	 * 
+	 */
+	public Invitation prepareInvitation(String invitedUserOrEmail, Player loggedPlayer) 
+			throws TeamServiceException;
+
+	/**
+	 * Sends invitation email
+	 * 
+	 * @param invitation
+	 */
+	public void sendInvitation(Invitation invitation);
+
+	/**
+	 * Creates an initial team for a new player
+	 * 
+	 * @return
+	 */
+	public Team createInitialTeam(Player player);
+
+}
---
+++ wars/test/test/services/UnitServiceImplTest.java
@@ -0,0 +1,201 @@
+package test.services;
+
+import java.util.List;
+
+import models.Place;
+import models.Player;
+import models.ResourceType;
+import models.Unit;
+import models.UnitType;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import services.api.PlaceService;
+import services.api.UnitService;
+import services.api.error.UnitServiceException;
+import services.impl.PlaceServiceImpl;
+import services.impl.UnitServiceImpl;
+import test.util.InjectorHelper;
+import test.util.SamplePlaces;
+
+import com.google.inject.Injector;
+
+import daos.PlaceDAO;
+import daos.PlayerDAO;
+import daos.UnitDAO;
+
+public class UnitServiceImplTest {
+
+	private static PlayerDAO playerDAO;
+	private static PlaceDAO placeDAO;
+	private static UnitDAO unitDAO;
+	private static PlaceService placeService;
+	private static UnitService unitService;
+
+	@BeforeClass
+	public static void before() {
+		Injector injector = InjectorHelper.getInjector();
+		Assert.assertNotNull(injector);
+
+		playerDAO = injector.getInstance(PlayerDAO.class);
+		placeDAO = injector.getInstance(PlaceDAO.class);
+		unitDAO = injector.getInstance(UnitDAO.class);
+		placeService = injector.getInstance(PlaceServiceImpl.class);
+		unitService = injector.getInstance(UnitServiceImpl.class);
+
+		Assert.assertNotNull(placeService);
+		Assert.assertNotNull(playerDAO);
+		Assert.assertNotNull(placeDAO);
+		Assert.assertNotNull(unitDAO);
+		Assert.assertNotNull(unitService);
+
+		// Generate testing data
+
+		// persist a player
+		Player player = new Player();
+		player.setUsername("mrbob");
+		player.setEmail("bob@bobson.de");
+		player.setName("Bobson");
+		player.setFirstname("Bob");
+
+		// give the player some resources
+		player.getResourceDepot().put(ResourceType.Credits, 1000);
+		player.getResourceDepot().put(ResourceType.Material, 1500);
+
+		playerDAO.save(player);
+		player = playerDAO.findOne("username", "mrbob");
+		
+		// conquer a place
+		// TUM was conquered by bob
+		Place tum = SamplePlaces.tum;
+		tum.getConqueredBy().add(player);
+		
+		placeDAO.save(tum);
+
+		// Bob has conquered the TUM
+		player.getConquered().add(tum);
+
+		playerDAO.save(player);
+		
+
+		Player load = playerDAO.findOne("username", "mrbob");
+		Assert.assertNotNull(load);
+		Assert.assertEquals(new Integer(1000),
+				load.getResourceDepot(ResourceType.Credits));
+		Assert.assertEquals(new Integer(1500),
+				load.getResourceDepot(ResourceType.Material));
+
+	}
+
+	@Test
+	public void buildUnitTest() {
+		Player load = playerDAO.findOne("username", "mrbob");
+
+		try {
+			unitService.buildUnit(load, UnitType.GRUNT, 5);
+		} catch (UnitServiceException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		load = playerDAO.findOne("username", "mrbob");
+
+		Assert.assertNotNull(load.getUnits());
+		Assert.assertNotNull(load.getResourceDepot(ResourceType.Credits));
+		Assert.assertNotNull(load.getResourceDepot(ResourceType.Material));
+		Assert.assertEquals(5, load.getUnits().size());
+		Assert.assertEquals(new Integer(500),
+				load.getResourceDepot(ResourceType.Credits));
+		Assert.assertEquals(new Integer(750),
+				load.getResourceDepot(ResourceType.Material));
+	}
+
+	@Test
+	public void deployableUnitsTest() {
+		Player load = playerDAO.findOne("username", "mrbob");
+
+		Integer deployableUnits = unitService.getNumberOfUndeployedUnits(load);
+		Integer deployableGrunts = unitService.getNumberOfUndeployedUnits(load,
+				UnitType.GRUNT);
+
+		Assert.assertNotNull(load);
+		Assert.assertNotNull(deployableUnits);
+		Assert.assertNotNull(deployableGrunts);
+		Assert.assertEquals(new Integer(5), deployableUnits);
+		Assert.assertEquals(new Integer(5), deployableGrunts);
+	}
+
+	@Test
+	public void deployUnitTest() {
+		Player load = playerDAO.findOne("username", "mrbob");
+
+		// Retriving a place by name is good for now
+		Place tum = placeDAO.findOne("name", "Technische Universität München");
+
+		try {
+			unitService.deployUnit(load, UnitType.GRUNT, 3, tum);
+		} catch (UnitServiceException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		load = playerDAO.findOne("username", "mrbob");
+		tum = placeDAO.findOne("name", "Technische Universität München");
+
+		Assert.assertEquals(3, tum.getDeployedUnits().size());
+		Assert.assertEquals(new Integer(2),
+				unitService.getNumberOfUndeployedUnits(load));
+		Assert.assertEquals(new Integer(3),
+				unitService.getNumberOfDeployedUnits(load));
+	}
+	
+	@Test
+	public void retrieveUnitTest() {
+		Player load = playerDAO.findOne("username", "mrbob");
+
+		// Retriving a place by name is good for now
+		Place tum = placeDAO.findOne("name", "Technische Universität München");
+		
+		try {
+			unitService.retrieveUnit(load, UnitType.GRUNT, 2, tum);
+		} catch (UnitServiceException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		tum = placeDAO.findOne("name", "Technische Universität München");
+		
+		Assert.assertEquals(1, tum.getDeployedUnits().size());
+		Assert.assertEquals(new Integer(4),
+				unitService.getNumberOfUndeployedUnits(load));
+		Assert.assertEquals(new Integer(1),
+				unitService.getNumberOfDeployedUnits(load));
+	}
+
+	/**
+	 * Delete all instances from the database
+	 * 
+	 * @author michi
+	 */
+	@AfterClass
+	public static void cleanDatabase() {
+		List<Player> players = playerDAO.find().asList();
+		List<Place> places = placeDAO.find().asList();
+		List<Unit> units = unitDAO.find().asList();
+
+		for (Player player : players) {
+			playerDAO.delete(player);
+		}
+
+		for (Place place : places) {
+			placeDAO.delete(place);
+		}
+		
+		for (Unit unit : units) {
+			unitDAO.delete(unit);
+		}
+	}
+}
