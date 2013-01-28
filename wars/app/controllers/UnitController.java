9d2fbf17e24bf3a641896e2290d1029916a08d8f
Peter Retzer <peter.retzer@gmail.com>
2013-01-28 21:35:02 +0100

    Building units is now possible (still missing: calculating the possible amount of units based on the available player resources and limiting the sliders to those amounts)

--- wars/app/controllers/UnitController.java
+++ wars/app/controllers/UnitController.java
@@ -4,11 +4,13 @@ import java.util.Map;
 
 import models.Player;
 import models.UnitType;
+import play.Logger;
 import play.mvc.Controller;
 import play.mvc.Result;
 import securesocial.core.java.SecureSocial;
 import services.api.AuthenticationService;
 import services.api.UnitService;
+import services.api.error.UnitServiceException;
 import util.JsonHelper;
 
 import com.google.common.collect.Maps;
@@ -45,5 +47,29 @@ public class UnitController extends Controller {
 		ret.put("overallUnits", overallUnitsMap);
 		
 		return ok(JsonHelper.toJson(ret));
+	}
+	
+	@SecureSocial.SecuredAction(ajaxCall=true)
+	public static Result buildUnits(String gruntAmount, String infantryAmount) {
+		Player player = authenticationService.getPlayer();
+		int ga = Integer.parseInt(gruntAmount);
+		int ia = Integer.parseInt(infantryAmount);
+		
+		Logger.debug("player tries to build " + gruntAmount + " new Grunts and " + infantryAmount + " new Infantry");
+
+		try {
+			if (ga > 0) {
+				unitService.buildUnit(player, UnitType.GRUNT, ga);
+			}
+			if (ia > 0) {
+				unitService.buildUnit(player, UnitType.INFANTRY, ia);
+			}
+			
+			return ok("ok");
+		} catch (UnitServiceException e) {
+			Logger.info(e.toString());
+			
+			return ok("error");
+		}
 	}
 }
