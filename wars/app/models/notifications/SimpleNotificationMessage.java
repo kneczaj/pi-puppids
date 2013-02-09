adebed267030e15450735caa664c6235a31602b1
kamil <kamil@kneczaj-hp.tu-muenchen.de>
2013-02-09 04:28:17 +0100

    notifications are displayed in notifications section All notifications moved to notification manager Added some functions needed to display history and undelivered

--- wars/app/models/notifications/SimpleNotificationMessage.java
+++ wars/app/models/notifications/SimpleNotificationMessage.java
@@ -2,14 +2,47 @@ package models.notifications;
 
 import org.codehaus.jackson.node.ObjectNode;
 
-import play.libs.Json;
-
 
 public class SimpleNotificationMessage extends Notification {
-
+	
+	private String message;
+	private String title;
+	private String type;
+	
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	public String getMessage() {
+		return message;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	public String getType() {
+		return type;
+	}
+	
 	@Override
 	public ObjectNode toJson() {
-		return Json.newObject();
+		ObjectNode notification = getInitialJson();
+
+		notification.put("messageType", "OtherNotification");
+		notification.put("title", getTitle());
+		notification.put("message", getMessage());
+		notification.put("type", getType());
+
+		return notification;
 	}
 
 }
